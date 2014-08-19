469fc6af91704a211dac0bac46e0d299d8da4bea
Chao Wang <cwang8023@gmail.com>
2014-08-19 18:08:02 +0800

    fix bug

--- rbMonitor.rb
+++ rbMonitor.rb
@@ -4,9 +4,9 @@ require 'facepp'
 include PiPiper
 
 IMAGE_DIRECTORY="/home/pi/test"
-api = FacePP.new '79467077fde305ede39bf25fb910eadd', '10w5PmBpaWUfi-z7S0dYmKwPaNQFmVJP'
 
 def detect_face file
+  api = FacePP.new '79467077fde305ede39bf25fb910eadd', '10w5PmBpaWUfi-z7S0dYmKwPaNQFmVJP'
   result = api.detection.detect img: file
   if result['session_id']
     `mv #{file} result['session_id'].jpg`
@@ -21,7 +21,7 @@ after :pin => 23, :goes => :high do
 
   file_name = "#{IMAGE_DIRECTORY}/person-#{Time.now.to_i}.jpg"
 	`sudo fswebcam #{file_name}`
-	`curl --request POST --data-binary @"ci"#{file_name}" --header "U-ApiKey: 824d86075f2b76ea2299951233a53528" http://api.yeelink.net/v1.1/device/13547/sensor/22418/datapoints`
+	`curl --request POST --data-binary @"#{file_name}" --header "U-ApiKey: 824d86075f2b76ea2299951233a53528" http://api.yeelink.net/v1.1/device/13547/sensor/22418/datapoints`
   detect_face file_name
         puts "++++++++++++++"
         puts Time.now.to_s
