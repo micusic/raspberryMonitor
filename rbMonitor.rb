bbd8904f22b6273c75699ab893f07db1141b53c5
Jingqiang ZHANG <jqzhang@thoughtworks.com>
2014-08-19 11:33:45 +0800

    detective human and upload photo to yeelink.

---
+++ rbMonitor.rb
@@ -0,0 +1,12 @@
+require 'pi_piper'
+include PiPiper
+
+after :pin => 23, :goes => :high do
+         f = File.open("log_piper.txt", "a")
+              f.puts(Time.now.to_s)
+                   f.close
+                        `sudo fswebcam /home/pi/shot.jpg`
+                             `curl --request POST --data-binary @"/home/pi/shot.jpg" --header "U-ApiKey: 824d86075f2b76ea2299951233a53528" http://api.yeelink.net/v1.1/device/13547/sensor/22401/datapoints`
+end
+
+PiPiper.wait
